c595a0efff992a5f84a80f1c3d97b2d6af5748c5
chaitanya chandra <chay@outlook.in>
2023-01-25 12:25:54 +0530

    automated commit #1617

--- code.sh
+++ code.sh
@@ -35,7 +35,7 @@ sudo yum install nginx -y
 
 yes | cp -rf files/nginx.conf /etc/nginx/nginx.conf
 yes | cp -rf files/nodejs.conf /etc/nginx/conf.d/nodejs.conf
-
+setenforce 0
 systemctl restart nginx
 # node .js > node.logs 2>&1 &
 # ps -ef | grep "index.js" > run.log
