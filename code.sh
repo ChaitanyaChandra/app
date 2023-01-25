b028d75933c76c899a48c9743220692a76f40c6a
chaitanya chandra <chay@outlook.in>
2023-01-25 11:57:06 +0530

    automated commit #1615

--- code.sh
+++ code.sh
@@ -3,13 +3,13 @@
 # only works on centos8 aarch64
 
 # install mongodb
-curl -s https://raw.githubusercontent.com/ChaitanyaChandra/DevOps/main/2.ANSIBLE/roles/mongodb/files/mongo.repo > /etc/yum.repos.d/mongodb-org-6.0.repo
+# curl -s https://raw.githubusercontent.com/ChaitanyaChandra/DevOps/main/2.ANSIBLE/roles/mongodb/files/mongo.repo > /etc/yum.repos.d/mongodb-org-6.0.repo
 
-dnf --disablerepo=AppStream install -y mongodb-org
+# dnf --disablerepo=AppStream install -y mongodb-org
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+# sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 
-systemctl restart mongod
+# systemctl restart mongod
 
 # setup user 
 adduser spec
@@ -26,16 +26,15 @@ cat package.sh | bash
 db_user=chaitu
 db_pass=123Chaitu
 
-echo MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db?retryWrites=true&w=majority >> files/spec.service
+echo Environment="MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db?retryWrites=true&w=majority" >> files/spec.service
 cp files/spec.service /etc/systemd/system/
 systemctl start spec
 
-sudo yum install epel-release
+sudo yum install epel-release -y
 sudo yum install nginx -y 
 
-mkdir /etc/nginx/
-cp files/nginx.conf /etc/nginx/nginx.conf
-cp files/nodejs.conf /etc/nginx/conf.d/nodejs.conf
+yes | cp -rf files/nginx.conf /etc/nginx/nginx.conf
+yes | cp -rf files/nodejs.conf /etc/nginx/conf.d/nodejs.conf
 
 systemctl restart nginx
 # node .js > node.logs 2>&1 &
