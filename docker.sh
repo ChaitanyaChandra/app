325083792a95eac996b8ff5b69a3f431f53fde4a
Chaitanya <56665907+ChaitanyaChandra@users.noreply.github.com>
2023-12-23 18:24:59 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,15 @@
-docker build -t docker.io/chaituchowdary/app:1.0 docker.io/chaituchowdary/app:latest .
-docker run -itd -p 8080:3000 --name=ServerOne -h ServerOne app:latest
-docker exec -it ServerOne /bin/bash
+# docker build -t docker.io/chaituchowdary/app:1.0 docker.io/chaituchowdary/app:latest .
+# docker run -itd -p 8080:3000 --name=ServerOne -h ServerOne app:latest
+# docker exec -it ServerOne /bin/bash
+
+db_user=chaitu
+db_pass=123Chaitu
+
+sudo docker run -d -t -i -e REDIS_NAMESPACE='staging' \ 
+-e MONGO_ENDPOINT='MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db retryWrites=true&w=majority' \
+-e ENV='dev' \
+-e APP_VERSION='1.0' \
+-e PORT='8080' \
+-p 80:8080 \
+-h ServerOne \
+--name ServerOne chaituchowdary/app:latest
