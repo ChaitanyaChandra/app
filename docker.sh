f7531af35aa62596eacc28bbcf76da7bd0002cf5
Chaitanya Chandra <Chay@outlook.in>
2023-12-23 17:07:11 +0530

    automated commit #2261

--- docker.sh
+++ docker.sh
@@ -1,2 +1,3 @@
-docker run -itd -p 8080:3000 --name=ServerOne -h ServerOne spec-app
+docker build -t docker.io/chaituchowdary/app:1.0 docker.io/chaituchowdary/app:latest .
+docker run -itd -p 8080:3000 --name=ServerOne -h ServerOne app:latest
 docker exec -it ServerOne /bin/bash
