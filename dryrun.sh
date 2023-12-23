1ff0e0c4170a979eb97d241ae0169912e29745f1
Chaitanya Chandra <Chay@outlook.in>
2023-12-23 18:55:12 +0530

    automated commit #2267

--- dryrun.sh
+++ dryrun.sh
@@ -3,7 +3,7 @@
 db_user=chaitu
 db_pass=123Chaitu
 
-export MONGO_ENDPOINT='mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db?retryWrites=true&w=majority'
+export MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db?retryWrites=true&w=majority
 export ENV='dev'
 export APP_VERSION='1.0'
 export PORT='8080'
