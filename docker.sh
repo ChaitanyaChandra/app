54d6fff7ac22f1c3b59cc90b201eede9733a1642
Chaitanya Chandra <Chay@outlook.in>
2023-12-23 18:47:46 +0530

    automated commit #2265

--- docker.sh
+++ docker.sh
@@ -6,7 +6,7 @@ db_user=chaitu
 db_pass=123Chaitu
 
 sudo docker run -d -t -i -e REDIS_NAMESPACE='staging' \ 
--e MONGO_ENDPOINT='MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db retryWrites=true&w=majority' \
+-e MONGO_ENDPOINT='mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db retryWrites=true&w=majority' \
 -e ENV='dev' \
 -e APP_VERSION='1.0' \
 -e PORT='8080' \
---
+++ dryrun.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+db_user=chaitu
+db_pass=123Chaitu
+
+export MONGO_ENDPOINT='mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db retryWrites=true&w=majority'
+export ENV='dev'
+export APP_VERSION='1.0'
+export PORT='8080'
+
+npm install
+node index.js
