36c4200cd7b4e11d4c078a1ecde34105dc091e9d
Chaitanya Chandra <Chay@outlook.in>
2023-12-23 21:44:04 +0530

    automated commit #2280

--- docker.sh
+++ docker.sh
@@ -6,12 +6,12 @@ db_pass=123Chaitu
 endpoint="mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db retryWrites=true&w=majority"
 
 docker run -d -t -i \
--e MONGO_ENDPOINT=$endpoint \
--e ENV='dev' \
--e APP_VERSION='1.0' \
--e PORT='9000' \
--p 8800:9000 \
--h ServerOne \
---name server-one chaituchowdary/app:latest
+  -e MONGO_ENDPOINT="$endpoint" \
+  -e ENV='dev' \
+  -e APP_VERSION='1.0' \
+  -e PORT='9000' \
+  -p 8800:9000 \
+  -h ServerOne \
+  --name server-one chaituchowdary/app:latest
 
 docker exec -it ServerOne /bin/bash
