e54a572d57530a557efae4e190c0f1b20bfceacc
Chaitanya Chandra <Chay@outlook.in>
2023-12-23 21:20:44 +0530

    automated commit #2271

--- dryrun.sh
+++ dryrun.sh
@@ -5,5 +5,5 @@ db_pass=123Chaitu
 MONGO_ENDPOINT="mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db?retryWrites=true&w=majority"
 ENV='dev'
 APP_VERSION='1.0'
-PORT='3000'
+PORT='9000'
 npm install && node index.js
